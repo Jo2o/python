55adf0728a376948f23cd1612eafb91a21d046eb
JozefMikus <jozo.mikus@gmail.com>
2021-11-09 22:37:44 +0100

    paddles move

--- 100daysudemy/pong/main.py
+++ 100daysudemy/pong/main.py
@@ -2,11 +2,11 @@ from turtle import Turtle, Screen
 import paddle
 import ball
 
-def setup_screen(scr):
-    scr.title("PING-PONG   >>> Jo2o <<<")
-    scr.colormode(255)
-    scr.bgcolor(0, 35, 35)
-    scr.setup(width=1424, height=790, startx=None, starty=50)
+def setup_screen(screen):
+    screen.title("PING-PONG   >>> Jo2o <<<")
+    screen.colormode(255)
+    screen.bgcolor(0, 35, 35)
+    screen.setup(width=1424, height=790, startx=None, starty=50)
 
 def setup_net():
     net = Turtle()
@@ -18,20 +18,27 @@ def setup_net():
     net.pendown()
     net.goto(0, -380)
 
-def setup_paddles(scr):
-    scr.register_shape(paddle.PADDLE_IMAGE_LEFT)
-    scr.register_shape(paddle.PADDLE_IMAGE_RIGHT)
-    pl = paddle.Paddle(paddle.Type.left)
-    pl.goto(-678, 0)
-    pr = paddle.Paddle(paddle.Type.right)
-    pr.goto(673, 0)
-    return pl, pr
+def setup_paddles(screen):
+    screen.tracer(0)
+    screen.register_shape(paddle.PADDLE_IMAGE_LEFT)
+    screen.register_shape(paddle.PADDLE_IMAGE_RIGHT)
+    paddle_left = paddle.Paddle(paddle.Type.left)
+    paddle_left.goto(-678, 0)
+    screen.onkey(paddle_left.move_up, "q")
+    screen.onkey(paddle_left.move_down, "a")
+    paddle_right = paddle.Paddle(paddle.Type.right)
+    paddle_right.goto(673, 0)
+    screen.onkey(paddle_right.move_up, "p")
+    screen.onkey(paddle_right.move_down, "l")
+    screen.tracer(1)
+    return paddle_left, paddle_right
 
-def setup_ball(scr):
-    scr.register_shape(ball.BALL_IMAGE)
+def setup_ball(screen):
+    screen.register_shape(ball.BALL_IMAGE)
     return ball.Ball()
 
-def setup_score():
+def setup_score(screen):
+    screen.tracer(0)
     left_score = Turtle()
     left_score.penup()
     left_score.hideturtle()
@@ -44,25 +51,22 @@ def setup_score():
     right_score.color(255, 255, 255)
     right_score.goto(-150, 345)
     right_score.write("0", font=('Courier New', 30, 'normal'), align='center')
+    screen.tracer(1)
     return left_score, right_score
 
 
 screen = Screen()
 setup_screen(screen)
 setup_net()
-
-# screen.tracer(0)
-setup_score()
-# screen.update()
+setup_score(screen)
 
 b = setup_ball(screen)
 (paddle_left, paddle_right) = setup_paddles(screen)
-# screen.onkey(lambda: turtle.forward(10), "w")
-
 
 screen.listen()
-game_over = False
+# game_over = False
 # while not game_over:
+#     pass
 
 
 
--- 100daysudemy/pong/paddle.py
+++ 100daysudemy/pong/paddle.py
@@ -13,11 +13,15 @@ class Paddle(Turtle):
     def __init__(self, paddle_type):
         super().__init__()
         self.penup()
-        self.speed(3)
+        self.speed(8)
         if paddle_type == Type.left:
             self.shape(PADDLE_IMAGE_LEFT)
         elif paddle_type == Type.right:
             self.shape(PADDLE_IMAGE_RIGHT)
 
+    def move_up(self):
+        self.sety(self.ycor() + 100)
 
+    def move_down(self):
+        self.sety(self.ycor() - 100)
 
